c7f6adb7a8cb375507d49f8f1f8176383c0a00dc
Miguel <miguelnilorosa@hotmail.com>
2025-11-27 17:05:42 -0300

    configurações inicias e primeiras rotas

---
+++ src/main/java/com/cinegraudos/repository/FuncoesRepository.java
@@ -0,0 +1,7 @@
+package com.cinegraudos.repository;
+
+import com.cinegraudos.domain.entity.FuncoesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncoesRepository extends JpaRepository<FuncoesEntity, Integer> {}
+
