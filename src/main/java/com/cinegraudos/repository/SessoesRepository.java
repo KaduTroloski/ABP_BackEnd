9ab17a3d151b08d7a89a6689dc2239940d526ada
Miguel <miguelnilorosa@hotmail.com>
2025-11-27 17:05:23 -0300

    configurações inicias e primeiras rotas

--- src/main/java/com/cinegraudos/repository/SessoesRepository.java
+++ src/main/java/com/cinegraudos/repository/SessoesRepository.java
@@ -1,7 +1,12 @@
 package com.cinegraudos.repository;
 
+import com.cinegraudos.domain.entity.SalasEntity;
 import com.cinegraudos.domain.entity.SessoesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
+
 public interface SessoesRepository extends JpaRepository<SessoesEntity, Integer> {
+    List<SessoesEntity> findBySalaAndHorarioInicioBetween(SalasEntity sala, Instant inicio, Instant fim);
 }
