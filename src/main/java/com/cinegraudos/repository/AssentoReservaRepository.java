8d9abc0ff41a1f9c621967959c7d6ab5d62c35a2
Miguel <miguelnilorosa@hotmail.com>
2025-11-27 17:04:54 -0300

    configurações inicias e primeiras rotas

--- src/main/java/com/cinegraudos/repository/AssentoReservaRepository.java
+++ src/main/java/com/cinegraudos/repository/AssentoReservaRepository.java
@@ -2,6 +2,14 @@ package com.cinegraudos.repository;
 
 import com.cinegraudos.domain.entity.AssentoReservaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AssentoReservaRepository extends JpaRepository<AssentoReservaEntity, Integer> {
+    @Query("SELECT ar.assento.id_assentos " +
+            "FROM AssentoReservaEntity ar " +
+            "WHERE ar.reserva.sessao.idSessoes = :idSessao")
+    List<Integer> findAssentosReservadosPorSessao(@Param("idSessao") Integer idSessao);
 }
